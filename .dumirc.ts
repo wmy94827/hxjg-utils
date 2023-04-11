42d688fc448896149fdc4ea60c9c398d7580c616
王明远 <wmy94827@163.com>
2023-04-11 15:13:30 +0800

    部署
    部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: '@hxjg/utils-dist',
+  base:"/hxjg-utils/",
+  publicPath:"/hxjg-utils/",
+  exportStatic: {},
+  outputPath: 'docs-dist',
   alias: {
     // 让插件指向当前目录下
     '@hxjg/utils': process.cwd() + '/src/index.ts',
